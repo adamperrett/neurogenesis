f58bb2d4cbe2225d34fb5b1866379f62183c1d17
adamgoodtime <adam.perrett@manchester.ac.uk>
2021-06-14 10:46:35 +0100

    chnages to batch run

--- tests/batch_run.py
+++ tests/batch_run.py
@@ -2,14 +2,14 @@ import os
 import subprocess
 import time
 
-sensitivity_width = [0.4, 0.3, 0.2]
+sensitivity_width = [0.25, 0.3, 0.35]
 activation_threshold = [0.0]
 error_threshold = [0.01]
 maximum_total_synapses = [1000000000]
 maximum_synapses = [600]
 input_spread = [0]
 activity_decay_rate = [0.999]
-number_of_seeds = [100, 500, 1000]
+number_of_seeds = [0, 100, 300]
 
 processes = []
 logs = []
